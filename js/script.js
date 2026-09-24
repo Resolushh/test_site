1d2d99f1de0018b2db62a656d07e969ba236422b
Resolushh <Resolushh@gmail.com>
2026-09-24 12:37:16 +0300

    fix: contact tag

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ if (leadForm) {
     event.preventDefault();
     if (typeof gtag === 'function') {
       gtag('event', 'generate_lead', {
-        lead_source: 'lead_form'
+        lead_source: 'contact_form'
       });
     }
     document.querySelector('#form-status').textContent =
